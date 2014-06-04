b71fa635e8be27d0afef2307e5605ba204ec6837
Philip Munksgaard <pmunksgaard@gmail.com>
2014-06-04 16:06:47 +0200

    Add renderContacts for rendering contacts

--- src/ContactDynamics/Sequential/Render.hs
+++ src/ContactDynamics/Sequential/Render.hs
@@ -16,6 +16,23 @@ renderDiscs scaler discs = do
         C.arc x y scaler 0 (2 * pi)
         C.stroke
 
+renderContacts :: Double -> [(Contact, Vector Double)] -> C.Render ()
+renderContacts scaler xs = do
+  C.setSourceRGB 0 0 0
+  C.setLineWidth 1
+  --
+  let scaled = map (\((cd, an), r) ->
+                        ((xpos cd * scaler, ypos cd * scaler),
+                         (xpos an * scaler, ypos an * scaler),
+                         r @>0)) xs
+  mapM_ renderImpulse scaled
+    where
+      renderImpulse ((x1, y1), (x2, y2), width) = do
+        C.moveTo x1 y1
+        C.setLineWidth $ render * 0.1
+        C.lineTo x2 y2
+  C.setLineWidth 1
+
 discsToSVG :: [Disc] -> FilePath -> IO ()
 discsToSVG discs filename =
   C.withSVGSurface filename 300 300 renderer
