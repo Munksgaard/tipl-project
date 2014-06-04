969f84bab93e4bcf0c3ae10a9bb0dd74e1298f11
Philip Munksgaard <pmunksgaard@gmail.com>
2014-06-04 11:32:14 +0200

    Move jacobi solver mechanics into JacobiSolver

---
+++ src/ContactDynamics/Sequential/JacobiSolver.hs
@@ -0,0 +1,32 @@
+module ContactDynamics.Sequential.JacobiSolver where
+
+import ContactDynamics.Sequential.Disc
+import ContactDynamics.Sequential.Contact
+
+import Numeric.LinearAlgebra
+import Numeric.LinearAlgebra.Util
+
+waa :: Contact -> Matrix Double -- 2x2 matrix
+waa (cd, an) =
+    trans h `multiply` m `multiply` h
+    where
+        h = contactMatrix (cd, an)
+        m = diagBlock [massM cd, massM an]
+
+wab :: Contact -> Contact -> Matrix Double -- 2x2 matrix
+wab (cd1, an1) (cd2, an2) =
+    trans h_alpha `multiply` m `multiply` h_beta
+    where
+      -- Normalize contacts: put the relevant disc as the candidate
+      alpha@(cd, _) = if cd1 == cd2 || cd1 == an2
+                      then (cd1, an1)
+                      else (an1, cd1)
+      beta = if cd == cd2 then (cd2, an2) else (an2, cd2)
+      h_alpha = takeRows 3 $ contactMatrix alpha -- 3x2 matrix
+      m = diagBlock [massM cd] -- 3x3 matrix
+      h_beta = takeRows 3 $ contactMatrix beta -- 3x2
+
+sumWab :: Contact -> [Contact] -> Matrix Double
+sumWab c cs =
+    let adj = map (wab c) $ adjContacts c cs
+    in foldr add (zeros 2 2) adj
