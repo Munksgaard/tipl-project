42d74ae367718890839ba5d0144e7491ab1223a7
Thorbjørn S. Kaiser <lazareth@gmail.com>
2014-06-07 20:35:45 +0200

    Finalized code to calculate the sum of the Wab * r products

--- src/ContactDynamics/Accelerate/BNLJSolver.hs
+++ src/ContactDynamics/Accelerate/BNLJSolver.hs
@@ -1,28 +1,51 @@
 module BNLJSolver where
 
-import Data.Array.Accelerate
+import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Interpreter as Backend
 
 type VectorList e = Array DIM2 e
-type Waa = Array DIM2 Double
-type Wabs = Array DIM3 Double
+type Wxx  = Array DIM2 Double
+type Wxxs = Array DIM3 Double
 
 -- Int               = Number of iterations
 -- [Disc]            = The discs in the scene
--- Vector Double     = The external force
--- VectorList Double = The resultant impulses on [Disc]
-bnljSolver :: Int -> [Disc] -> Vector Double -> VectorList Double
-bnljSolver maxIter ds extF = undefined
+-- Vector Double     = The external force, a 2x1 vector
+-- VectorList Double = The resultant impulses on [Disc], a list of 2x1 vectors
+-- UNCOMMENTED because of types
+--bnljSolver :: Int -> [Disc] -> Vector Double -> VectorList Double
+--bnljSolver maxIter ds extF = undefined
 
--- Acc (Vector Double)     = The external force
--- Acc (Wabs)              = The Wab delassus operators for this specific RHS
--- Acc (VectorList Double) = The previously calculated impulses for each contact b corrosponding to the Wabs
--- Acc (Vector Double)     = The right hand side
-calcRHS :: Acc (Vector Double) -> Acc (Wabs) -> Acc (VectorList Double) -> Acc (Vector Double)
-calcRHS extF wabs impulses = undefined
+---- Calculate right hand side
+-- Acc (Vector Double)     = The external force, a 2x1 vector
+-- Acc (Wxxs)              = The Wab delassus operators for this specific RHS, a Nx2x2 matrix where N is the number of operators
+-- Acc (VectorList Double) = The previously calculated impulses for each contact b corrosponding to the Wabs, a list of 2x1 vectors
+-- Acc (Vector Double)     = The right hand side, a 2x1 vector
+calcRHS :: Acc (Vector Double) -> Acc (Wxxs) -> Acc (VectorList Double) -> Acc (Vector Double)
+calcRHS extF wabs impulses = extF `vadd` (wabs `wXr` impulses)
 
--- Acc (Waa)           = The inverse of the Waa for this specific RHS
--- Acc (Vector Double) = The right hand side
--- Acc (Vector Double) = The resultant impulse for the contact for this specific RHS
-solveRHS Acc (Waa) -> Acc (Vector Double) -> Acc (Vector Double)
+---- Solve right hand side
+-- Acc (Wxx)           = The inverse of the Waa for this specific RHS, a 2x2 vector
+-- Acc (Vector Double) = The right hand side, a 2x1 vector
+-- Acc (Vector Double) = The resultant impulse for the contact for this specific RHS, a 2x1
+solveRHS :: Acc (Wxx) -> Acc (Vector Double) -> Acc (Vector Double)
 solveRHS inWaa rhs = undefined
+
+---- Calculate matrix-vector product sums of Wab and impulses rs
+wXr :: Acc (Wxxs) -> Acc (VectorList Double) -> Acc (Vector Double)
+wXr wabs' rs' = fold (+) d0 $ reshape sh2x2 rowSum
+  where
+    rowSum   = fold (+) d0 products
+    products = transpose $ reshape rsSh $ A.zipWith (*) wabs rs
+    wabs     = flatten wabs'
+    rs       = flatten $ A.replicate repSh rs'
+    repSh    = lift $ Z :.All :.i2 :.All
+    rsSh     = lift $ Z :. rows :.i4
+    rows     = indexHead $ shape $ transpose rs'
+    sh2x2    = lift $ Z :.i2 :.i2
+    d0       = constant 0 :: Exp Double
+    i2       = constant 2 :: Exp Int
+    i4       = constant 4 :: Exp Int
+
+---- Calculate vector addition
+vadd :: (Elt e, IsNum e) => Acc (Vector e) -> Acc (Vector e) -> Acc (Vector e)
+vadd a b = A.zipWith (+) a b
