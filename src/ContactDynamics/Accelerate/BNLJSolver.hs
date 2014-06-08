4b14187357cd67ec3fbb86912e982c791f00427a
Thorbjørn S. Kaiser <lazareth@gmail.com>
2014-06-08 22:06:58 +0200

    Further flattened data parallelization such that all contact impulses are calculated in lockstep for each iteration.'

--- src/ContactDynamics/Accelerate/BNLJSolver.hs
+++ src/ContactDynamics/Accelerate/BNLJSolver.hs
@@ -1,11 +1,13 @@
 module BNLJSolver where
 
+import ContactDynamics.Disc
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Interpreter as Backend
 
-type VectorList e = Array DIM2 e
-type Wxx  = Array DIM2 Double
-type Wxxs = Array DIM3 Double
+type VectorList e     = Array DIM2 e
+type Wxx              = Array DIM2 Double
+type Wxxs             = Array DIM3 Double
+type Wxxss            = Array DIM4 Double
 
 i0 = constant 0 :: Exp Int
 i1 = constant 1 :: Exp Int
@@ -21,45 +23,70 @@ d4 = constant 4.0 :: Exp Double
 
 -- Int               = Number of iterations
 -- [Disc]            = The discs in the scene
--- Vector Double     = The external force, a 2x1 vector
--- VectorList Double = The resultant impulses on [Disc], a list of 2x1 vectors
--- UNCOMMENTED because of types
---bnljSolver :: Int -> [Disc] -> Vector Double -> VectorList Double
---bnljSolver maxIter ds extF = undefined
+-- VectorList Double = The external force, a Nx2 vector-matrix
+-- VectorList Double = The resultant impulses on [Disc], a Nx2 vector-matrix
+bnljSolver :: Int -> [Disc] -> VectorList Double -> VectorList Double
+bnljSolver maxIter ds extF = Backend.run (iter' solve rs)
+  where
+    iter'       = iter maxIter
+    solve       = solveRHS' . calcRHS'
+    solveRHS'   = solveRHS inWass
+    calcRHS'    = calcRHS numContacts extF' wabss
+    extF'       = use extF
+    inWass      = undefined
+    wabss       = undefined
+    numContacts = undefined
+    rs          = undefined
+
+iter :: Int -> (Acc (VectorList Double) -> Acc (VectorList Double)) -> Acc (VectorList Double) -> Acc (VectorList Double)
+iter 0 _ rs = rs
+iter n solve rs = iter (n-1) solve (solve rs)
 
 ---- Calculate right hand side
--- Acc (Vector Double)     = The external force, a 2x1 vector
--- Acc (Wxxs)              = The Wab delassus operators for this specific RHS, a Nx2x2 matrix where N is the number of operators
--- Acc (VectorList Double) = The previously calculated impulses for each contact b corrosponding to the Wabs, a list of 2x1 vectors
--- Acc (Vector Double)     = The right hand side, a 2x1 vector
-calcRHS :: Acc (Vector Double) -> Acc (Wxxs) -> Acc (VectorList Double) -> Acc (Vector Double)
-calcRHS extF wabs impulses = extF `vadd` (wabs `wXr` impulses)
+-- Exp Int                 = N: A constant that tells us the total number of contacts
+-- Acc (VectorList Double) = The external force, a Nx2 vector-matrix
+-- Acc (Wxxss)             = The Wab delassus operators for ALL RHS, a NxNx2x2 matrix where N is the number of contacts
+-- Acc (VectorList Double) = The previously calculated impulses for each contact, a Nx2 vector-matrix
+-- Acc (VectorList Double) = The right hand sides for all the contacts, a Nx2 vector-matrix
+calcRHS :: Exp Int -> Acc (VectorList Double) -> Acc (Wxxss) -> Acc (VectorList Double) -> Acc (VectorList Double)
+calcRHS n extF wabss rs = extF `vsadd` (wabss `wXr'` rs)
+  where
+    wXr' = wXr n
 
 ---- Solve right hand side
--- Acc (Vector Double) = The inverse of the Waa for this specific RHS,
---                       since Waa is a diagonal this is a 2x1 vector
--- Acc (Vector Double) = The right hand side, a 2x1 vector
--- Acc (Vector Double) = The resultant impulse for the contact for this specific RHS, a 2x1
-solveRHS :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Vector Double)
-solveRHS inWaa rhs = cond ?| (r', z0)
-  where
-    cond = rhs A.!! i1 >* d0
-    r' = A.zipWith (*) inWaa rhs
-    z0 = lift $ fromList (Z :. (2 :: Int)) [0,0]
+-- Acc (VectorList Double) = The inverse of the Waa for each contact,
+--                           since Waa is a diagonal this is a Nx2 vector-matrix
+-- Acc (VectorList Double) = The right hand side for each contact, a Nx2 vector-matrix
+-- Acc (VectorList Double) = The resultant impulse for each contact, a Nx2 vector-matrix
+solveRHS :: Acc (VectorList Double) -> Acc (VectorList Double) -> Acc (VectorList Double)
+solveRHS inWaas rhss = undefined
 
----- Calculate matrix-vector product sums of Wab and impulses rs
-wXr :: Acc (Wxxs) -> Acc (VectorList Double) -> Acc (Vector Double)
-wXr wabs' rs' = fold (+) d0 $ reshape sh2x2 rowSum
+-- solveRHS inWaa rhs = cond ?| (r', z0)
+--   where
+--     cond = rhs A.!! i1 >* d0
+--     r' = A.zipWith (*) inWaa rhs
+--     z0 = lift $ fromList (Z :. (2 :: Int)) [0,0]
+
+---- Calculate matrix-vector product sums of wabs and impulses for each contact AT THE SAME TIME
+-- arg  1: N: A constant that tells us the total number of contacts
+-- arg  2: This is where I start going insane.
+--         N rows of N wabs that are each 2x2 for a total of a NxNx2x2 matrix in row major order,
+--         ie. internally it goes first contact all wabs, second contact all wabs, etc. with all the dimensions
+-- arg  3: A list of the previous impulses for each contact
+-- result: a list of the matrix-vector product sums of wabs and impulses for each contact
+wXr :: Exp(Int) -> Acc(Wxxss) -> Acc(VectorList Double) -> Acc(VectorList Double)
+wXr n wabss' rs' = columnSums
   where
-    rowSum   = fold (+) d0 products
-    products = transpose $ reshape rsSh $ A.zipWith (*) wabs rs
-    wabs     = flatten wabs'
-    rs       = flatten $ A.replicate repSh rs'
-    repSh    = lift $ Z :.All :.i2 :.All
-    rsSh     = lift $ Z :. rows :.i4
-    rows     = indexHead $ shape $ transpose rs'
-    sh2x2    = lift $ Z :.i2 :.i2
-   
----- Calculate vector addition
-vadd :: (Elt e, IsNum e) => Acc (Vector e) -> Acc (Vector e) -> Acc (Vector e)
-vadd a b = A.zipWith (+) a b
+    columnSums = fold (+) d0 $ reshape nx2x2 rowSums
+    rowSums    = fold (+) d0 $ reshape i4tnxnSh products
+    products   = transpose $ reshape ntnxi4Sh $ A.zipWith (*) wabs rs
+    wabs       = flatten wabss'
+    rs         = flatten $ A.replicate repSh rs'
+    repSh      = lift $ Z :.n      :.All    :.i2 :.All
+    ntnxi4Sh   = lift $ Z :.(n*n)  :.i4
+    i4tnxnSh   = lift $ Z :.(i4*n) :.n
+    nx2x2      = lift $ Z :.n      :.i2 :.i2
+
+---- Calculate vector-matrix addition
+vsadd :: (Elt e, IsNum e) => Acc (VectorList e) -> Acc (VectorList e) -> Acc (VectorList e)
+vsadd a b = A.zipWith (+) a b
